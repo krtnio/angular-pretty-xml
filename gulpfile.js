236e68a562bd489a28742ce67e703ffca8147106
Nikita Khomyakov <dr.evil@krtn.eu>
2015-06-02 15:53:04 +0100

    gulp: add gulp support

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+var gulp   = require('gulp'),
+    uglify = require('gulp-uglify'),
+    del    = require('del');
+
+gulp.task('minify', function () {
+    return gulp.src('src/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('clean', function (cb) {
+    del('dist', cb);
+});
+
+gulp.task('build', ['clean', 'minify']);
+
+gulp.task('default', ['build']);
