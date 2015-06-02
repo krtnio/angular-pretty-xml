f978156be0bc32847e86d3a632c58e0845f0c17a
Nikita Khomyakov <dr.evil@krtn.eu>
2015-06-02 15:35:12 +0100

    refactor: update parsing algorithm Despite the fact that this has just been refactored, it'll be completely rethought in next version
    Original algorithm was taken from vkbeautify project (https://code.google.com/p/vkbeautify/) though this is not the best way to process XML for prettifying.
    
    Slight changes to the original were made to address performance of the function, however, it still doesn't achieve expected results.

--- angular-pretty-xml.js
+++ angular-pretty-xml.js
@@ -1,126 +1,76 @@
 // credits to vkbeautify (https://code.google.com/p/vkbeautify/)
+
+function createShiftArr (step) {
+    var space = '';
+    if (isNaN(parseInt(step))) // argument is string
+        space = step;
+    else // argument is integer
+        for (var i = 0; i < step; i++)
+            space += ' ';
+
+    var shift = ['\n']; // array of shifts
+
+    for (var ix = 0; ix < 100; ix++)
+        shift.push(shift[ix] + space);
+
+    return shift;
+}
+
+function prettify (xml, indent) {
+    if (typeof indent === 'number') {
+        if (indent !== 0)
+            indent = indent || 2;
+    } else if (typeof indent !== 'string')
+        indent = 2;
+
+    var arr = xml.replace(/>\s*</gm, '><')
+        .replace(/</g, '~::~<')
+        .replace(/\s*xmlns([=:])/g, '~::~xmlns$1')
+        .split('~::~');
+
+    var len       = arr.length,
+        inComment = false,
+        depth     = 0,
+        string    = '',
+        shift     = createShiftArr(indent);
+
+    for (var i = 0; i < len; i++) {
+        // start comment or <![CDATA[...]]> or <!DOCTYPE //
+        if (arr[i].indexOf('<!') !== -1) {
+            string += shift[depth] + arr[i];
+            inComment = true;
+
+            // end comment or <![CDATA[...]]> //
+            if (arr[i].indexOf('-->') !== -1 || arr[i].indexOf(']>') !== -1 ||
+                arr[i].indexOf('!DOCTYPE') !== -1) {
+                inComment = false;
+            }
+        } else if (arr[i].indexOf('-->') !== -1 || arr[i].indexOf(']>') !== -1) { // end comment  or <![CDATA[...]]> //
+            string += arr[i];
+            inComment = false;
+        } else if (/^<\w/.test(arr[i - 1]) && /^<\/\w/.test(arr[i]) && // <elm></elm> //
+                   /^<[\w:\-\.,]+/.exec(arr[i - 1]) === /^<\/[\w:\-\.,]+/.exec(arr[i])[0].replace('/', '')) { // fixme WTF?
+            string += arr[i];
+            if (!inComment) depth--;
+        } else if (arr[i].search(/<\w/) !== -1 && arr[i].indexOf('</') === -1 && arr[i].indexOf('/>') === -1) // <elm> //
+            string = !inComment ? string += shift[depth++] + arr[i] : string += arr[i];
+        else if (arr[i].search(/<\w/) !== -1 && arr[i].indexOf('</') !== -1) // <elm>...</elm> //
+            string = !inComment ? string += shift[depth] + arr[i] : string += arr[i];
+        else if (arr[i].indexOf('</') !== -1) // </elm> //
+            string = !inComment ? string += shift[--depth] + arr[i] : string += arr[i];
+        else if (arr[i].indexOf('/>') !== -1) // <elm/> //
+            string = !inComment ? string += shift[depth] + arr[i] : string += arr[i];
+        else if (arr[i].indexOf('<?') !== -1) // <? xml ... ?> //
+            string += shift[depth] + arr[i];
+        else if (arr[i].indexOf('xmlns:') > -1 || arr[i].indexOf('xmlns=') > -1) // xmlns //
+            string += shift[depth] + arr[i];
+        else
+            string += arr[i];
+    }
+
+    return string.trim();
+}
 angular.module('prettyXml', [])
     .filter('prettyXml', function () {
-        return function (xml, indent) {
-            if (indent !== 0)
-                indent = indent || 2;
-
-            var ar = xml.replace(/>\s{0,}</g, "><")
-                    .replace(/</g, "~::~<")
-                    .replace(/\s*xmlns\:/g, "~::~xmlns:")
-                    .replace(/\s*xmlns\=/g, "~::~xmlns=")
-                    .split('~::~'),
-                len = ar.length,
-                inComment = false,
-                deep = 0,
-                str = '',
-                shift = createShiftArr(indent);
-
-            for (ix = 0; ix < len; ix++) {
-                // start comment or <![CDATA[...]]> or <!DOCTYPE //
-                if (ar[ix].search(/<!/) > -1) {
-                    str += shift[deep] + ar[ix];
-                    inComment = true;
-                    // end comment  or <![CDATA[...]]> //
-                    if (ar[ix].search(/-->/) > -1 || ar[ix].search(/\]>/) > -1 || ar[ix].search(/!DOCTYPE/) > -1) {
-                        inComment = false;
-                    }
-                } else
-                // end comment  or <![CDATA[...]]> //
-                if (ar[ix].search(/-->/) > -1 || ar[ix].search(/\]>/) > -1) {
-                    str += ar[ix];
-                    inComment = false;
-                } else
-                // <elm></elm> //
-                if (/^<\w/.exec(ar[ix - 1]) && /^<\/\w/.exec(ar[ix]) &&
-                    /^<[\w:\-\.\,]+/.exec(ar[ix - 1]) == /^<\/[\w:\-\.\,]+/.exec(ar[ix])[0].replace('/', '')) {
-                    str += ar[ix];
-                    if (!inComment) deep--;
-                } else
-                // <elm> //
-                if (ar[ix].search(/<\w/) > -1 && ar[ix].search(/<\//) == -1 && ar[ix].search(/\/>/) == -1) {
-                    str = !inComment ? str += shift[deep++] + ar[ix] : str += ar[ix];
-                } else
-                // <elm>...</elm> //
-                if (ar[ix].search(/<\w/) > -1 && ar[ix].search(/<\//) > -1) {
-                    str = !inComment ? str += shift[deep] + ar[ix] : str += ar[ix];
-                } else
-                // </elm> //
-                if (ar[ix].search(/<\//) > -1) {
-                    str = !inComment ? str += shift[--deep] + ar[ix] : str += ar[ix];
-                } else
-                // <elm/> //
-                if (ar[ix].search(/\/>/) > -1) {
-                    str = !inComment ? str += shift[deep] + ar[ix] : str += ar[ix];
-                } else
-                // <? xml ... ?> //
-                if (ar[ix].search(/<\?/) > -1) {
-                    str += shift[deep] + ar[ix];
-                } else
-                // xmlns //
-                if (ar[ix].search(/xmlns\:/) > -1 || ar[ix].search(/xmlns\=/) > -1) {
-                    str += shift[deep] + ar[ix];
-                }
-
-                else {
-                    str += ar[ix];
-                }
-            }
-
-            return (str[0] == '\n') ? str.slice(1) : str;
-
-            function createShiftArr (step) {
-                var space = '    ';
-                if (isNaN(parseInt(step))) {  // argument is string
-                    space = step;
-                } else { // argument is integer
-                    switch (step) {
-                        case 1:
-                            space = ' ';
-                            break;
-                        case 2:
-                            space = '  ';
-                            break;
-                        case 3:
-                            space = '   ';
-                            break;
-                        case 4:
-                            space = '    ';
-                            break;
-                        case 5:
-                            space = '     ';
-                            break;
-                        case 6:
-                            space = '      ';
-                            break;
-                        case 7:
-                            space = '       ';
-                            break;
-                        case 8:
-                            space = '        ';
-                            break;
-                        case 9:
-                            space = '         ';
-                            break;
-                        case 10:
-                            space = '          ';
-                            break;
-                        case 11:
-                            space = '           ';
-                            break;
-                        case 12:
-                            space = '            ';
-                            break;
-                    }
-                }
-
-                var shift = ['\n']; // array of shifts
-
-                for (var ix = 0; ix < 100; ix++) {
-                    shift.push(shift[ix] + space);
-                }
-
-                return shift;
-            }
-        }
+        return prettify;
     });
