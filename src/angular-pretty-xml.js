5fc616e25d97a0d94278e7923019b83e2d490474
Nikita Khomyakov <dr.evil@krtn.eu>
2015-07-01 16:44:28 +0100

    chore: code optimizations

--- src/angular-pretty-xml.js
+++ src/angular-pretty-xml.js
@@ -27,10 +27,10 @@
     }
 
     function prettify (xml, indent) {
-        if (typeof indent === 'number') {
+        if (isFinite(indent)) {
             if (indent !== 0)
                 indent = indent || 2;
-        } else if (typeof indent !== 'string')
+        } else if (!angular.isString(indent))
             indent = 2;
 
         var arr = xml.replace(/>\s*</gm, '><')
@@ -38,11 +38,11 @@
             .replace(/\s*xmlns([=:])/g, '~::~xmlns$1')
             .split('~::~');
 
-        var len       = arr.length,
+        var len = arr.length,
             inComment = false,
-            depth     = 0,
-            string    = '',
-            shift     = createShiftArr(indent);
+            depth = 0,
+            string = '',
+            shift = createShiftArr(indent);
 
         for (var i = 0; i < len; i++) {
             // start comment or <![CDATA[...]]> or <!DOCTYPE //
@@ -59,20 +59,20 @@
                 string += arr[i];
                 inComment = false;
             } else if (/^<\w/.test(arr[i - 1]) && /^<\/\w/.test(arr[i]) && // <elm></elm> //
-                       /^<[\w:\-\.,]+/.exec(arr[i - 1]) === /^<\/[\w:\-\.,]+/.exec(arr[i])[0].replace('/', '')) { // fixme WTF?
+                /^<[\w:\-\.,]+/.exec(arr[i - 1]) === /^<\/[\w:\-\.,]+/.exec(arr[i])[0].replace('/', '')) { // fixme WTF?
                 string += arr[i];
                 if (!inComment) depth--;
             } else if (arr[i].search(/<\w/) !== -1 && arr[i].indexOf('</') === -1 && arr[i].indexOf('/>') === -1) // <elm> //
-                string = !inComment ? string += shift[depth++] + arr[i] : string += arr[i];
+                string += !inComment ? (shift[depth++] + arr[i]) : arr[i];
             else if (arr[i].search(/<\w/) !== -1 && arr[i].indexOf('</') !== -1) // <elm>...</elm> //
-                string = !inComment ? string += shift[depth] + arr[i] : string += arr[i];
-            else if (arr[i].indexOf('</') !== -1) // </elm> //
-                string = !inComment ? string += shift[--depth] + arr[i] : string += arr[i];
+                string += !inComment ? shift[depth] + arr[i] : arr[i];
+            else if (arr[i].search(/<\//) > -1) // </elm> //
+                string += !inComment ? shift[--depth] + arr[i] : arr[i];
             else if (arr[i].indexOf('/>') !== -1) // <elm/> //
-                string = !inComment ? string += shift[depth] + arr[i] : string += arr[i];
+                string += !inComment ? shift[depth] + arr[i] : arr[i];
             else if (arr[i].indexOf('<?') !== -1) // <? xml ... ?> //
                 string += shift[depth] + arr[i];
-            else if (arr[i].indexOf('xmlns:') > -1 || arr[i].indexOf('xmlns=') > -1) // xmlns //
+            else if (arr[i].indexOf('xmlns:') !== -1 || arr[i].indexOf('xmlns=') !== -1) // xmlns //
                 string += shift[depth] + arr[i];
             else
                 string += arr[i];
